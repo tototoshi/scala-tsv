eecfb3201b9d200c27478ba6122d9a91d1c4002a
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-12-19 19:35:29 +0900

    println削除

--- src/test/scala/com/github/tototoshi/tsv/TSVSpec.scala
+++ src/test/scala/com/github/tototoshi/tsv/TSVSpec.scala
@@ -8,17 +8,14 @@ class TSVSpec extends Spec {
 
     it ("should work fine!") {
       val data = "foo" :: "bar" :: "baz" :: Nil
-      println(TSV(data))
       assert(TSV(data) === "foo	bar	baz")
     }
 
     it ("should support backslash escape") {
       val data = "foo" :: "bar" :: "	baz" :: Nil
-      println(TSV(data))
       assert(TSV(data) === "foo	bar	\\	baz")
 
       val data2 = "foo" :: "bar" :: "\\baz" :: Nil
-      println(TSV(data2))
       assert(TSV(data2) === "foo	bar	\\\\baz")
     }
   }
